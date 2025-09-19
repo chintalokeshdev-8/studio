5ba836f34cda84d487360d1fa5a34beb83d2e5d0
Lokesh Babu chinta <lokeshbabu9298@gmail.com>
2025-09-19 07:03:29 +0000

    just show stcomch preganancy lady

--- src/components/icons/pregnant-lady-icon.tsx
+++ src/components/icons/pregnant-lady-icon.tsx
@@ -14,13 +14,8 @@ export function PregnantLadyIcon(props: React.SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <circle cx="12" cy="5" r="2" />
-      <path d="M12 7v1l-2 5h4l-2 -5" />
-      <path d="M14 12v8" />
-      <path d="M10 12v8" />
-      <path d="M10 15h4" />
-      <path d="M10.5 12c-2.5 0-4.5 2-4.5 4.5" />
-      <path d="M13.5 12c2.5 0 4.5 2 4.5 4.5" />
+      <circle cx="12" cy="12" r="10" />
+      <path d="M16 12a4 4 0 0 1-8 0" />
     </svg>
   );
 }
